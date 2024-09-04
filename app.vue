290427776feac206ee726516996c7f8192fe6f7d
Mike Lenz <lenz@cando-solutions.com>
2024-09-04 19:37:11 +0200

    Two sections created

--- app.vue
+++ app.vue
@@ -1,7 +1,36 @@
 <template>
-    <div>
+    <div class="group/main">
+        <div id="cursor" class="group-hover/main:bg-white rounded-full absolute top-0 left-0  z-[60] pointer-events-none -translate-x-1/2 -translate-y-1/2" ref="cursor"></div>
         <NuxtLayout>
             <NuxtPage/>
         </NuxtLayout>
     </div>
 </template>
+
+<script lang="ts" setup>
+const cursor = ref<HTMLElement | null>(null)
+const {x,y} = useMouse()
+
+let lastPosition = { x: 0, y: 0 }
+const updateCursor = () => {
+    (cursor.value as HTMLElement).style.left = x.value + 'px';
+    (cursor.value as HTMLElement).style.top = y.value + 'px';
+
+    const velocity = Math.sqrt((x.value - lastPosition.x) ** 2 + (y.value - lastPosition.y) ** 2);
+
+    (cursor.value as HTMLElement).style.width = useClamp(12-velocity,4,8) + 'px';
+    (cursor.value as HTMLElement).style.height = useClamp(12-velocity,4,8)  + 'px';
+
+    lastPosition = { x: x.value, y: y.value };
+}
+
+onMounted(() => {
+    window.addEventListener('mousemove', updateCursor);
+})
+</script>
+
+<style>
+*{
+    cursor: none
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="graph-paper min-h-screen overflow-x-hidden bg-gray-950">
         <section id="Home">
-            <UContainer class="pt-32" :ui="{ constrained: 'max-w-[96rem]' }">
+            <UContainer
+                class="group/bento pt-32"
+                :ui="{ constrained: 'max-w-[96rem]' }"
+            >
                 <!-- Bento Grid -->
                 <div
                     class="animate__animated animate__fadeIn relative z-10 w-full xl:h-[800px]"
@@ -15,7 +18,7 @@
                     >
                         <!-- 1 -->
                         <div
-                            class="animate__animated animate__fadeIn animate__delay-1s b1 bento-card group z-10 flex flex-col items-center justify-between overflow-hidden xl:hover:scale-150 xl:hover:shadow-2xl"
+                            class="animate__animated animate__fadeIn animate__delay-1s b1 bento-card center-card group z-10 flex flex-col items-center justify-between overflow-hidden xl:hover:scale-150 xl:hover:shadow-2xl"
                         >
                             <!-- Top -->
                             <div
@@ -84,9 +87,50 @@
                         ></div>
                         <!-- 4 -->
                         <div
-                            class="animate__animated animate__fadeInDown b4 bento-card"
+                            class="animate__animated animate__fadeInDown b4 bento-card group relative flex flex-col overflow-hidden"
                             style="animation-delay: 1.4s"
-                        ></div>
+                        >
+                            <!-- Card BG -->
+                            <div
+                                class="absolute left-0 top-0 flex h-full w-full flex-col justify-end"
+                            >
+                                <div
+                                    class="w-full rounded-lg bg-black transition-all group-hover:grow"
+                                ></div>
+                            </div>
+                            <UIcon
+                                name="i-heroicons-academic-cap-solid"
+                                class="absolute left-1/2 size-16 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-100 transition-all group-hover:size-96 group-hover:opacity-5"
+                            />
+                            <div
+                                class="z-10 flex h-full w-full flex-col justify-between p-4 opacity-0 transition group-hover:opacity-100"
+                            >
+                                <div>
+                                    <h2
+                                        class="flex items-baseline text-4xl font-bold"
+                                    >
+                                        EDU & EXP
+                                        <div
+                                            class="h-1 grow rounded bg-gradient-to-r from-transparent to-white"
+                                        ></div>
+                                    </h2>
+                                    <p
+                                        class="mt-4 max-w-md text-lg text-gray-300"
+                                    >
+                                        I began coding in 2018 as part of my computer science education, which is nearing completion as I’m now in the final semester of my bachelor's degree.
+                                    </p>
+                                </div>
+                                <!-- stats -->
+                                <div
+                                    class="flex justify-end text-sm text-gray-500"
+                                >
+                                    <span>
+                                        Coding Experience:
+                                        <b>6 years</b>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                         <!-- 5 -->
                         <div
                             class="animate__animated animate__fadeInUp b5 bento-card"
@@ -94,9 +138,56 @@
                         ></div>
                         <!-- 6 -->
                         <div
-                            class="animate__animated animate__fadeInRight b6 bento-card"
+                            class="animate__animated animate__fadeInRight b6 bento-card group relative flex flex-col overflow-hidden"
                             style="animation-delay: 1.6s"
-                        ></div>
+                        >
+                            <!-- Card BG -->
+                            <div
+                                class="absolute left-0 top-0 flex h-full w-full flex-col justify-end"
+                            >
+                                <div
+                                    class="w-full rounded-lg bg-gradient-to-t from-[#3db684] to-[#00DC82] transition-all group-hover:grow"
+                                ></div>
+                            </div>
+                            <img
+                                :src="'/nuxt.svg'"
+                                alt="nuxt logo"
+                                class="absolute left-1/2 top-1/2 w-16 -translate-x-1/2 -translate-y-1/2 opacity-100 transition-all group-hover:w-96 group-hover:opacity-20"
+                            />
+                            <div
+                                class="z-10 flex h-full w-full flex-col justify-between p-4 opacity-0 transition group-hover:opacity-100"
+                            >
+                                <div>
+                                    <h2
+                                        class="flex items-baseline text-6xl font-bold"
+                                    >
+                                        NUXT
+                                        <div
+                                            class="h-1 grow rounded bg-gradient-to-r from-transparent to-white"
+                                        ></div>
+                                    </h2>
+                                    <p
+                                        class="mt-4 max-w-md text-lg text-emerald-100"
+                                    >
+                                        Since early 2023 I have been using Nuxt
+                                        for almost every project of mine. It has
+                                        quickly become my main framework because
+                                        of the impeccable DX and its approach to
+                                        a complete web solution.
+                                    </p>
+                                </div>
+                                <!-- stats -->
+                                <div
+                                    class="flex justify-end text-sm text-emerald-100"
+                                >
+                                    <span>
+                                        Experience:
+                                        <b>almost 2 years</b>
+                                    </span>
+                                </div>
+                                
+                            </div>
+                        </div>
                     </div>
                     <!-- Details -->
                     <div
@@ -115,7 +206,7 @@
 
                 <!-- Adornments -->
                 <div
-                    class="flex justify-between"
+                    class="flex select-none items-center justify-between"
                     :style="{
                         transform: `translate(${5 - cornerOffsetX * 10}px, ${5 - cornerOffsetY * 10}px)`,
                     }"
@@ -125,8 +216,21 @@
                         style="animation-delay: 1s"
                     >
                         GRID :
-                        <b class="text-gray-300">BENTO ARRANGEMENT</b>
+                        <ClientOnly>
+                            <b class="text-gray-300">
+                                {{
+                                    width > 1280
+                                        ? "BENTO ARRANGEMENT"
+                                        : "LIST ARRANGEMENT"
+                                }}
+                            </b>
+                        </ClientOnly>
                     </span>
+                    <div
+                        class="mx-4 flex h-1 grow items-center justify-center rounded bg-white text-xs font-bold text-transparent transition-all sm:h-0.5 sm:w-32 sm:grow-0 sm:group-hover/bento:h-1 sm:group-hover/bento:grow sm:group-hover/bento:bg-gray-800 sm:group-hover/bento:text-white"
+                    >
+                        IDK WHAT TO PUT HERE
+                    </div>
                     <span
                         class="animate__animated animate__fadeInRight animate__delay-1s flex w-40 justify-between gap-2 text-xs text-gray-600"
                         style="animation-delay: 1.1s"
@@ -192,7 +296,11 @@ const cornerOffsetY = computed(() =>
 }
 
 .bento-card {
-    @apply rounded-lg bg-gray-800 shadow transition hover:bg-gray-700;
+    @apply rounded-lg bg-gray-800 shadow transition;
+}
+
+.center-card {
+    @apply hover:bg-gray-700;
 }
 
 @media screen and (min-width: 1280px) {
